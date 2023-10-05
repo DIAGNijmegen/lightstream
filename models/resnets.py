7993f82e7b2094a5790acc6629910124fe138f10
stephan <stephandooper@hotmail.com>
2023-10-05 18:46:49 +0200

    Small changes

--- models/resnets.py
+++ models/resnets.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 import torch
 import lightning as L
 import torch.nn as nn
+from lightning.pytorch.utilities.types import OptimizerLRScheduler, STEP_OUTPUT
 
 from src.stream import StreamingModule
 from torchvision.models import resnet18, resnet34, resnet50
-
 
 class StreamingResNet(L.LightningModule):
     model_choices = {"resnet18": resnet18, "resnet34": resnet34, "resnet50": resnet50}
@@ -15,6 +17,7 @@ class StreamingResNet(L.LightningModule):
         self.model = StreamingResNet.model_choices[model_name](weights="IMAGENET1K_V1")
         self.stream_net, self.head = self.split_model()
         self.stream_net = StreamingModule(self.stream_net, tile_size)
+
 
     def split_model(self):
         stream_net = nn.Sequential(
@@ -30,15 +33,31 @@ class StreamingResNet(L.LightningModule):
         head = nn.Sequential(self.model.avgpool, nn.Flatten(), self.model.fc)
         return stream_net, head
 
-    def train_step(self):
+    def training_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
         pass
+
 
     def forward(self, x, *args, **kwargs):
         x = self.stream_net(x)
         return self.head(x)
 
+    def configure_optimizers(self) -> OptimizerLRScheduler:
+        return torch.optim.SGD(self.model.parameters(), lr=0.1)
+
+
 
 if __name__ == "__main__":
     model = StreamingResNet("resnet18", tile_size=1600)
+    model.eval()
     inputs = torch.ones((1, 3, 3200, 3200))
-    print(model.stream_net)
+
+    output_start = model(inputs)
+
+    test_model = resnet18(weights="IMAGENET1K_V1")
+    test_model.eval()
+    output_test = test_model(inputs)
+
+    print(torch.sum(output_test - output_start))
+
+
+    # Check backward loop
--- src/stream.py
+++ src/stream.py
@@ -1,30 +1,29 @@
+from typing import Any
+
+from lightning.pytorch.utilities.types import STEP_OUTPUT
+
 from scnn import StreamingCNN
 import torch
 import lightning as L
 
 
 class StreamingModule(L.LightningModule):
-    def __init__(self, network, tile_size, *args, **kwargs):
+    def __init__(self, network, tile_size, train_streaming_layers=False, *args, **kwargs):
         super().__init__()
-        self.network = self.convert_to_streaming_network(network, tile_size, **kwargs)
+        self.stream_network = self.convert_to_streaming_network(network, tile_size)
+        self.train_streaming_layers = train_streaming_layers
 
     def freeze_normalization_layers(self):
         """Do not use normalization layers within streaming"""
-        self.freeze_layers = [
+        freeze_layers = [
             l for l in self.network.modules() if isinstance(l, torch.nn.BatchNorm2d)
         ]
-        for mod in self.freeze_layers:
+        for mod in freeze_layers:
             mod.eval()
 
-    def freeze_streaming_network(self):
-        """
-        Freeze the parameters of the entire streaming network
-        """
-        pass
-
-    def convert_to_streaming_network(self, model, tile_size, **kwargs):
-        model = StreamingCNN(
-            model,
+    def convert_to_streaming_network(self, network, tile_size, **kwargs):
+        stream_net = StreamingCNN(
+            network,
             tile_shape=(1, 3, tile_size, tile_size),
             deterministic=kwargs.get("deterministic", True),
             saliency=kwargs.get("saliency", False),
@@ -32,13 +31,20 @@ class StreamingModule(L.LightningModule):
             copy_to_gpu=kwargs.get("copy_to_gpu", True),
             verbose=kwargs.get("verbose", True),
         )
+        return stream_net
 
-        model = self.freeze_normalization_layers(model)
-
-        return model
+    def backward_image(self, x, gradient):
+        if self.train_streaming_layers:
+            self.stream_network.backward(x, gradient[None])
 
     def forward(self, x):
-        return self.network(x)
+        return self.stream_network(x)
+
+    def training_step(self, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        pass
+
+
+
 
 
 if __name__ == "__main__":
