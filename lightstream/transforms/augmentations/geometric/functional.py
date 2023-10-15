a4cdeb2189f6849d38204b965c02c1a574d5b253
stephandooper <stephandooper@hotmail.com>
2023-10-15 23:02:32 +0200

    Finished functioned for color deconvolution

--- lightstream/transforms/augmentations/geometric/functional.py
+++ lightstream/transforms/augmentations/geometric/functional.py
@@ -5,6 +5,14 @@ Functional representations of the augmentations. Most will function as functiona
 """
 
 import pyvips
+import numpy as np
+
+# Needed for stain unmixing
+rgb_from_hed = np.array([[0.65, 0.70, 0.29],
+                         [0.07, 0.99, 0.11],
+                         [0.27, 0.57, 0.78]])
+hed_from_rgb = np.linalg.inv(rgb_from_hed)
+
 
 
 def vflip(img: pyvips.Image) -> pyvips.Image:
@@ -60,3 +68,47 @@ def transpose(img: pyvips.Image) -> pyvips.Image:
     """
 
     return img.rot270()
+
+
+def separate_stains_pyvips(rgb: pyvips.Image):
+    # adapted from https://github.com/scikit-image/scikit-image/blob/v0.22.0/skimage/color/colorconv.py#L1638
+    # https://github.com/libvips/pyvips/issues/289
+    # https://github.com/libvips/pyvips/issues/294
+
+    # convert uint8 to [0,1] float 32
+    if rgb.format == 'uchar':
+        # dividing by 255 yields faithful results to skimage
+        # colourspace('scrgb') is recommended by jcupitt, but yields more intense colours
+
+        # rgb = rgb.cast('double') / 255
+        rgb = rgb.colourspace("scrgb")
+    elif rgb.format not in ('float', 'double'):
+        raise TypeError("format must be one of uchar [0,255], float [0, 1], or double [0,1]")
+
+    pyvips_image = (rgb < 1E-6).ifthenelse(1E-6, rgb)  # Avoiding log artifacts
+    log_adjust = np.log(1E-6)  # used to compensate the sum above
+    stains = (pyvips_image.log() / log_adjust)
+
+    stains = stains.recomb(hed_from_rgb.T.tolist())
+    stains = (stains < 0).ifthenelse(0, stains)
+    return stains
+
+
+def combine_stains_pyvips(hed):
+    if hed.format == 'uchar':
+        # hed = hed.cast('float') / 255
+        hed = rgb.colourspace("scrgb")
+    elif hed.format not in ('float', 'double'):
+        raise TypeError("format must be one of uchar [0,255], float [0, 1], or double [0,1]")
+
+    # log_adjust here is used to compensate the sum within separate_stains().
+    log_adjust = -np.log(1E-6)
+    log_hed = -(hed * log_adjust)
+
+    log_rgb = log_hed.recomb(rgb_from_hed.T.tolist())
+
+    rgb = log_rgb.exp()
+
+    rgb = (rgb < 0).ifthenelse(0, rgb)
+    rgb = (rgb > 1).ifthenelse(1, rgb)
+    return rgb
--- models/resnet.py
+++ models/resnet.py
@@ -8,6 +8,7 @@ from torchvision.models import resnet18, resnet34, resnet50
 
 
 def split_resnet(net):
+
     stream_net = nn.Sequential(
         net.conv1,
         net.bn1,
@@ -49,89 +50,5 @@ class StreamingResNet(BaseModel):
             **kwargs
         )
 
-
-if __name__ == "__main__":
-    # Batchnorm can have non-empty grads, conv2d layers should be none after mod.eval()
-
-    # Any input that plays nicely with the output stride, i.e. multiples of 32
-    img_size = 2048
-    model = StreamingResNet(
-        "resnet18",
-        tile_size=1504,
-        loss_fn=torch.nn.MSELoss(),
-        train_streaming_layers=True,
-        use_streaming=True,
-        saliency=True,
-        gather_input_gradients=True,
-    )
-
-    dtype = torch.double  # test with double precision
-    model.stream_network.stream_module.type(dtype)
-    model.stream_network.stream_module.cuda()
-
-    model.head.type(dtype)
-    model.head.cuda()
-
-    model.stream_network.device = "cuda"
-    model.stream_network.dtype = dtype
-
-    model.freeze_streaming_normalization_layers()
-
-    # %%
-    image = torch.FloatTensor(3, img_size, img_size).normal_(0, 1)
-    image = image.type(dtype)
-    image = image.cuda()
-
-    target = torch.tensor(50.0)  # large value so we get larger gradients
-    target = target.type(dtype)
-    target = target.cuda()
-
-    fmap_streaming, stream_output, loss = model.training_step((image[None], target), 1)
-
-    loss.backward()
-    model.backward_streaming(image[None], fmap_streaming.grad)
-
-    streaming_conv_gradients = []
-
-    for i, layer in enumerate(model.stream_network.stream_module.modules()):
-        if isinstance(layer, StreamingConv2d):
-            if layer.weight.grad is not None:
-                streaming_conv_gradients.append(layer.weight.grad.clone())
-
-    model.disable_streaming()
-    model.use_streaming = False
-
-    for i, layer in enumerate(model.stream_network.stream_module.modules()):
-        if isinstance(layer, torch.nn.Conv2d):
-            print(layer)
-            if layer.weight.grad is not None:
-                layer.weight.grad.data.zero_()
-                layer.bias.grad.data.zero_()
-
-    conventional_gradients = []
-    inps = []
-
-    def save_grad(module, grad_in, grad_out):
-        global conventional_gradients
-        conventional_gradients.append(grad_out[0].clone())
-
-    for i, layer in enumerate(model.stream_network.stream_module.modules()):
-        if isinstance(layer, torch.nn.Conv2d):
-            layer.register_backward_hook(save_grad)
-
-    image.requires_grad = True
-    conventional_fmap, conventional_output, loss = model.training_step(
-        (image[None], target), 1
-    )
-
-    conventional_output.max()
-
-    max_error = torch.abs(stream_output - conventional_output).max().item()
-
-    if max_error < 1e-7:
-        print("Equal output to streaming")
-    else:
-        print("NOT equal output to streaming"),
-        print("error:", max_error)
-
-    loss.backward()
+if __name__ == '__main__':
+    print(torch.cuda.is_available())
