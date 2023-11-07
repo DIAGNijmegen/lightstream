eb1690862bbcccee12087c436d849ed248f741d7
Stephan <31624652+stephandooper@users.noreply.github.com>
2023-11-07 22:26:53 +0100

    Added crop, blur, motionblur

--- lightstream/run_augmentations.py
+++ lightstream/run_augmentations.py
@@ -31,6 +31,7 @@ import random
 # np.random.seed(0)
 
 import cv2
+
 BOX_COLOR = (255, 0, 0)  # Red
 TEXT_COLOR = (255, 255, 255)  # White
 
@@ -62,44 +63,22 @@ def visualize(image, bboxes, category_ids, category_id_to_name):
         class_name = category_id_to_name[category_id]
         img = visualize_bbox(img, bbox, class_name)
     plt.figure(figsize=(12, 12))
-    plt.axis('off')
+    plt.axis("off")
     plt.imshow(img)
     plt.show()
 
 
-
 if __name__ == "__main__":
-    image = np.array(Image.open("../images/coco_cat_dog.jpg"))
+    image = np.array(Image.open("../images/he.jpg"))
 
-    bboxes = [[5.66, 138.95, 147.09, 164.88], [366.7, 80.84, 132.8, 181.84]]
-    category_ids = [17, 18]
-
-    # We will use the mapping from category_id to the class name
-    # to visualize the class label for the bounding box on the image
-    category_id_to_name = {17: 'cat', 18: 'dog'}
-
-    visualize(image, bboxes, category_ids, category_id_to_name)
-
+    plt.imshow(image)
+    plt.show()
 
     pyvips_image = pyvips.Image.new_from_array(image)
-    transforms = Compose(
-        [
-            PadIfNeeded(
-                min_height=None,
-                min_width=None,
-                pad_height_divisor=240,
-                pad_width_divisor=240,
-                position="north-east",
-                border_mode="background",
-                p=1.0,
-                value=[255, 255, 255],
-                mask_value=[0,0,0]
-            )
-        ],bbox_params=A.BboxParams(format='coco', label_fields=['category_ids']),
-        is_check_shapes=True,
-    )
+    transforms = Compose([Rotate(), HEDShift(p=0.5), VerticalFlip(), HorizontalFlip()], is_check_shapes=True)
 
-    sample = {"image": pyvips_image, "bboxes": bboxes, "category_ids":category_ids}
+    sample = {"image": pyvips_image}
     new_image = transforms(**sample)
-    # out_image = new_image["image"].numpy()
-    #
+
+    plt.imshow(new_image["image"].numpy())
+    plt.show()
