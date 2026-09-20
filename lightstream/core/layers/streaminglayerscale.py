Fix streamed LayerScale dependency gradients

lightstream/core/layers/streaminglayerscale.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from torch.amp import custom_bwd, custom_fwd
 
-from lightstream.core.scnn.utils import Box, H_DIM, Lost, W_DIM, _new_value_indices
+from lightstream.core.scnn.utils import Box, Lost
 
 
 def _normalize_weight_shape(shape: int | Iterable[int] | torch.Size) -> torch.Size:
@@ -84,74 +84,9 @@ class StreamingLayerScaleF(torch.autograd.Function):
         grad_weight = None
 
         if ctx.needs_input_grad[1]:
-            if grad_output.ndim < 4:
-                grad_weight = (
-                    (grad_output * inpt).sum_to_size(weight.shape).to(dtype=weight.dtype)
-                )
-                return grad_in, grad_weight, None, None, None, None
-
-            input_loc = ctx.input_loc
-            sides = input_loc.sides if input_loc is not None else None
-            grad_lost = ctx.grad_lost
-            seen_indices = ctx.seen_indices
-
-            lost_top = grad_lost.top if not (sides is not None and sides.top) else 0
-            lost_bottom = (
-                grad_lost.bottom if not (sides is not None and sides.bottom) else 0
+            grad_weight = (
+                (grad_output * inpt).sum_to_size(weight.shape).to(dtype=weight.dtype)
             )
-            lost_left = grad_lost.left if not (sides is not None and sides.left) else 0
-            lost_right = (
-                grad_lost.right if not (sides is not None and sides.right) else 0
-            )
-
-            valid_grad = grad_output[
-                :,
-                :,
-                lost_top : grad_output.shape[H_DIM] - lost_bottom,
-                lost_left : grad_output.shape[W_DIM] - lost_right,
-            ]
-
-            if input_loc is None:
-                data_loc = Box(lost_top, 0, lost_left, 0, sides)
-            else:
-                output_stride = ctx.output_stride
-                stride_h = (
-                    int(output_stride[1].item())
-                    if isinstance(output_stride, torch.Tensor)
-                    else int(output_stride[1])
-                )
-                stride_w = (
-                    int(output_stride[2].item())
-                    if isinstance(output_stride, torch.Tensor)
-                    else int(output_stride[2])
-                )
-                data_loc_y = int(input_loc.y // stride_h) + lost_top
-                data_loc_x = int(input_loc.x // stride_w) + lost_left
-                data_loc = Box(data_loc_y, 0, data_loc_x, 0, input_loc.sides)
-
-            new_output_box, updated_total_indices = _new_value_indices(
-                valid_grad.shape, data_loc, seen_indices
-            )
-            seen_indices.y = updated_total_indices.y
-            seen_indices.height = updated_total_indices.height
-            seen_indices.x = updated_total_indices.x
-            seen_indices.width = updated_total_indices.width
-            seen_indices.sides = updated_total_indices.sides
-
-            if new_output_box.height > 0 and new_output_box.width > 0:
-                y0 = lost_top + new_output_box.y
-                y1 = y0 + new_output_box.height
-                x0 = lost_left + new_output_box.x
-                x1 = x0 + new_output_box.width
-                relevant_grad = grad_output[:, :, y0:y1, x0:x1]
-                relevant_input = inpt[:, :, y0:y1, x0:x1]
-                grad_weight = (
-                    (relevant_grad * relevant_input)
-                    .sum_to_size(weight.shape)
-                    .to(dtype=weight.dtype)
-                )
-            else:
-                grad_weight = torch.zeros_like(weight)
 
         return grad_in, grad_weight, None, None, None, None
 